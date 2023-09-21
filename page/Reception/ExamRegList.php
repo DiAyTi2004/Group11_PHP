cf7e7032335e997e90d7e42097bf85b0ef7aba93
tutai307 <tutaihocgioi@gmail.com>
2023-09-21 20:51:24 +0700

    test

--- page/Reception/ExamRegList.php
+++ page/Reception/ExamRegList.php
@@ -1,8 +1,229 @@
-<div>
-    this is exam registration list
+<link rel="stylesheet" href="./styles/Reception/ExamRegListStyle.css">
+    <body>
+        <div class="header flex justify-between align-center h-48">
+            <div class="header-logo ml-2">
+                <div class="header-logo-icon br-10 mr-1 pointer">
+                    <i class="fa-solid fa-bars fa-xl "></i>
+                </div>
+                <label>Phòng tiếp đón bệnh nhân</label>
+            </div>
 
-    <i class="fa-solid fa-earth-asia"></i>
-    <i class="fa fa-car fa-lg"></i>
-    <i class="fas fa-search"></i>
-</div>
+            <div class="header-title">
+                <h2>Bệnh viện mắt Bình Thuận</h2>
+            </div>
+
+            <div class="header-module mr-2">
+                <div class="header-logo-icon br-10">
+                    <i class="fa-solid fa-atom fa-xl"></i>
+                    <label>Chọn modules</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="content-all br-10 p-3">
+                <div class="content-search mt-2 pb-3 mb-2">
+                    <div class="content-search-heading p-2">
+                        <label class = "pl-2">Tìm kiếm phiếu đăng ký khám</label>
+                    </div>
+
+                    <div class="content-search-form flex justify-between align-center">
+                        <div class="form_input">
+                            <span class = "ml-2 mt-2 mb-2">Nhập tên bệnh nhân</span>
+                            <div class="input-date br-10">
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="form_input">
+                            <span class = "ml-2 mt-2 mb-2">Từ ngày</span>
+                            <div class="input-date br-10">
+                                <input type="text">
+                                <i class="fa-solid fa-calendar-days mr-2"></i>
+                            </div>
+                        </div>
+                        <div class="form_input">
+                            <span class = "ml-2 mt-2 mb-2">Đến ngày</span>
+                            <div class="input-date br-10">
+                                <input type="text">
+                                <i class="fa-solid fa-calendar-days mr-2"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content-search-last flex-center mt-3 mb-3">
+                        <div class="content-search-buts">
+                            <div class="content-but h-32">
+                                <p>
+                                    <i class="fa-solid fa-magnifying-glass"></i>
+                                    TÌM KIẾM
+                                </p>
+                            </div>
+
+                            <div class="content-but h-32">
+                                <p>
+                                    <i class="fa-solid fa-plus"></i>
+                                    THÊM MỚI BỆNH NHÂN
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+    
+                </div>
+
+                <div class="content-list">
+                    <div class="content-list-heading p-2">
+                        <label class = "pl-2">
+                        Danh sách phiếu đăng ký khám - Tổng số 414 phiếu
+                        </label>
+                    </div>
+
+                    <div class="list-table h-800">
+                        <table>
+                            <tr>
+                                <th class="heading">STT</th>
+                                <th class="heading">Họ tên</th>
+                                <th class="heading">Thời gian tạo phiếu</th>
+                                <th class="heading">Phòng khám</th>
+                                <th class="heading">Dịch vụ khám</th>
+                                <th class="heading">Loại dịch vụ</th>
+                                <th class="heading">Thao tác</th>
+                            </tr>
+
+                            <tr class ="bc-fff">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-ccc">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-fff">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-ccc">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-fff">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-ccc">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-fff">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-ccc">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+
+                            <tr class ="bc-fff">
+                                <td>91</td>
+                                <td>Hoàng Anh Thư</td>
+                                <td>31/08/2023 09:54</td>
+                                <td>Phòng khám 1</td>
+                                <td>Khám bác sĩ thông thường</td>
+                                <td>Khám mắt</td>
+                                <td>
+                                    <i class="fa-solid fa-print"></i>
+                                    <i class="fa-solid fa-pencil"></i>
+                                    <i class="fa-solid fa-trash"></i>
+                                </td>
+                            </tr>
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+
+        </div>
+    </body>
+
 
