246e5c365144f496f59f5e606f8af5370763c8d5
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-09-21 14:54:28 +0700

    commit common style

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="./styles//CommonStyle.css">
+<link rel="stylesheet" href="./styles/Reception/ExamRegListStyle.css">
 <body>
     <?php
         require "./page/Reception/ExamRegList.php";
     ?>
+    <h1 class="p-4"></h1>
 </body>
 </html>
