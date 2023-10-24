477d83365c627c0b6724e396acd92c86f80f5113
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-10-24 23:58:06 +0700

    update login admin

--- admin/adminCommon/Login.php
+++ admin/adminCommon/Login.php
@@ -3,7 +3,7 @@ session_start();
 include('../../common/config/Connect.php');
 
 if (isset($_POST['dangnhap'])) {
-    $taikhoan = $_POST['usernamez'];
+    $taikhoan = $_POST['username'];
     $matkhau = md5($_POST['password']);
     $sql_nguoidung = "SELECT * FROM tbl_dangky ,tbl_admin WHERE (tbl_dangky.taikhoan='" . $taikhoan . "' AND tbl_dangky.matkhau='" . $matkhau . "' AND tbl_dangky.chucvu=1) OR (tbl_admin.username='" . $taikhoan . "' AND tbl_admin.password='" . $matkhau . "' ) LIMIT 1";
     $row_nguoidung = mysqli_query($connect, $sql_nguoidung);
@@ -28,23 +28,29 @@ if (isset($_POST['dangnhap'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style_login.css">
+    <link rel="stylesheet" href="../styles/LoginStyles.css">
     <title>ĐĂNG NHẬP</title>
 </head>
 
+<link rel="stylesheet" href="../../common//css/CommonStyle.css">
+
 <body>
-    <div class="top_link">
-        <a href="../../index.php">
-            <img src="https://drive.google.com/u/0/uc?id=16U__U5dJdaTfNGobB_OpwAJ73vM50rPV&export=download" alt="">Về
-            Trang chủ
+    <div class="top_link flex-center">
+        <a href="../../index.php" class="p-2 bg-white">
+            <img src="https://drive.google.com/u/0/uc?id=16U__U5dJdaTfNGobB_OpwAJ73vM50rPV&export=download" alt="">
+            Về trang chủ
         </a>
     </div>
-    
+
     <form class="login" action="" method="POST">
-        <h2 style="text-align: center">ADMIN LOGIN</h2>
-        <input type="text" placeholder="Username" name="usernamez">
-        <input type="password" placeholder="Password" name="password">
-        <button type="submit" name="dangnhap">Login</button>
+        <h2 class="text-center m-4">Welcome administrator</h2>
+        <label for="username">Username: </label>
+        <input type="text" placeholder="Username" name="username" id="username">
+        <label for="password">Password: </label>
+        <input type="password" placeholder="Password" name="password" id="password">
+        <div class="flex-center justify-right">
+            <button type="submit" name="dangnhap">Login</button>
+        </div>
     </form>
 
 </body>
