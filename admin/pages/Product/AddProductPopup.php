42c8314da688f581c8930c78a246b3df66abc5f3
tutai307 <tutaihocgioi@gmail.com>
2023-10-26 14:41:45 +0700

    fix common css and form

--- admin/pages/Product/AddProductPopup.php
+++ admin/pages/Product/AddProductPopup.php
@@ -2,14 +2,14 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-header bg-dark">
+                <h5 class="text-center text-white">Thêm sản phẩm</h5>
+                <button type="button" class="btn-close btn-close-white" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <table border="1" width="100%" padding="10px" style="border-collapse: collapse;">
                     <form id="productForm" method="POST" action="pages/Product/ProductLogic.php"
                         enctype="multipart/form-data">
-                        <legend style="text-align: center;">Thêm sản phẩm</legend>
                         <tr>
                             <td class="row">
                                 <div class="mb-2 col">
@@ -53,7 +53,7 @@
                                     <textarea name="tomtat" class="form-control" id="exampleFormControlTextarea1"
                                         rows="3"></textarea>
                                 </div>
-                    
+
                                 <div class="mb-2 col">
                                     <label for="exampleFormControlTextarea1" class="form-label">Nội dung</label>
                                     <textarea name="noidung" class="form-control" id="exampleFormControlTextarea1"
@@ -85,23 +85,23 @@
                                 </div>
 
                                 <div class="mb-2 col">
-                                <label for="exampleFormControlInput1" class="form-label">Tình trạng</label>
-                                <select name="hienthi" class="form-select" aria-label="Default select example">
-                                    <?php
+                                    <label for="exampleFormControlInput1" class="form-label">Tình trạng</label>
+                                    <select name="hienthi" class="form-select" aria-label="Default select example">
+                                        <?php
                                     $sql_danhmuc = "SELECT * FROM tbl_danhmuc ORDER BY id_danhmuc DESC";
                                     $query_danhmuc = mysqli_query($connect, $sql_danhmuc);
                                     while ($row_danhmuc = mysqli_fetch_array($query_danhmuc)) {
                                     ?>
-                                    <!--dùng value thêm danh mục dựa vào địa chỉ id_danhmuc -->
-                                    <option value="<?php echo $row_danhmuc['id_danhmuc'] ?>">
-                                        <?php echo $row_danhmuc['tendanhmuc'] ?>
-                                    </option>
+                                        <!--dùng value thêm danh mục dựa vào địa chỉ id_danhmuc -->
+                                        <option value="<?php echo $row_danhmuc['id_danhmuc'] ?>">
+                                            <?php echo $row_danhmuc['tendanhmuc'] ?>
+                                        </option>
 
 
-                                    <?php
+                                        <?php
                                     }
                                     ?>
-                                </select>
+                                    </select>
                                 </div>
                             </td>
                         </tr>
@@ -120,14 +120,15 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 text-right">
-                                    <button class="btn btn-primary mt-3" type="submit" name="themsanpham">Thêm sản
-                                        phẩm</button>
-                                </div>
                             </td>
                         </tr>
                     </form>
                 </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary pt-2 pb-2" data-bs-dismiss="modal">Đóng</button>
+
+                <button type="submit" class="btn btn-primary" name="themsanpham">Thêm sản phẩm</button>
             </div>
         </div>
     </div>
