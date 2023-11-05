d60b54b59468d652eb1983b9854b9896caf9e41e
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-11-05 20:01:22 +0700

    fix edit path

--- admin/adminCommon/AdminRouter.php
+++ admin/adminCommon/AdminRouter.php
@@ -1,29 +1,29 @@
 <div class="flex-grow-1">
     <div class="container my-4">
         <?php
-        if ($workingPage == 'category') {
+        if ($workingPage == 'category' && $query == 'edit') {
+            include("./pages/Category/EditCategoryPopup.php");
+        } else if ($workingPage == 'category') {
             include("./pages/Category/AddCategoryPopup.php");
             include("./pages/Category/CategoryIndex.php");
             include("./pages/Category/CategoryConfirmDeletePopup.php");
-        } else if ($workingPage == 'category' && $query == 'edit') {
-            include("./pages/Category/EditCategoryPopup.php");
+        } else if ($workingPage == 'product' && $query == 'edit') {
+            include("./pages/Product/EditProductPopup.php");
         } else if ($workingPage == 'product') {
             include("./pages/Product/ProductIndex.php");
             include("./pages/Product/AddProductPopup.php");
             include("./pages/Product/ProductConfirmDeletePopup.php");
-        } else if ($workingPage == 'product' && $query == 'edit') {
-            include("./pages/Product/EditProductPopup.php");
+        } else if ($workingPage == 'event' && $query == 'edit') {
+            include("./pages/Event/EditEventPopup.php");
         } else if ($workingPage == 'event') {
             include("./pages/Event/EventIndex.php");
             include("./pages/Event/AddEventPopup.php");
             include("./pages/Event/EventConfirmDeletePopup.php");
-        } else if ($workingPage == 'event' && $query == 'edit') {
-            include("./pages/Event/EditEventPopup.php");
+        } else if ($workingPage == 'user' && $query == 'edit') {
+            include("./pages/User/EditUserPopup.php");
         } else if ($workingPage == 'user') {
             include("./pages/User/UserIndex.php");
             include("./pages/User/AddUserPopup.php");
-        } else if ($workingPage == 'user' && $query == 'edit') {
-            include("./pages/User/EditUserPopup.php");
         } else if ($workingPage == 'order') {
             include("./pages/Order/OrderIndex.php");
         } else if ($workingPage == 'order' && $query == 'edit') {
