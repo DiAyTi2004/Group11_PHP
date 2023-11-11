8c0e5298123759201413d282e4df2b3d958a2270
BaVi Cao <114662682+bavilv123@users.noreply.github.com>
2023-11-11 13:51:29 +0700

    Sửa order

--- admin/pages/Order/OrderConfirmDelete.php
+++ admin/pages/Order/OrderConfirmDelete.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="confirmPopup_<?php echo $row['id_cart'];?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="confirmPopup_<?php echo $row['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header bg-dark">
@@ -6,16 +6,17 @@
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-0">
-                <form id="orderForm" method="POST" action="pages/Order/OrderLogic.php?ordertId=<?php echo $row['id_cart'];?>" enctype="multipart/form-data">
+                <form id="productForm" method="POST" action="pages/Order/OrderLogic.php?orderId=<?php echo $row['id']; ?>" enctype="multipart/form-data">
                     <table border="1" width="100%" padding="10px" style="border-collapse: collapse;">
                         <p class="p-4 m-0">
-                            Bạn có chắc chắn muốn xóa đơn hàng  này? <b><?php echo $row['code_cart']; ?></b>
+                            Bạn có chắc chắn muốn xóa đơn hàng này? <b><?php echo $row['fullname']; ?></b>
                         </p>
                     </table>
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-secondary pt-2 pb-2" data-bs-dismiss="modal">Hủy</button>
-                        <button type="submit" class="btn btn-primary" name="delete">Xác nhận</button>
+
+                        <button type="submit" class="btn btn-primary" name="deleteOrder">Xác nhận</button>
                     </div>
                 </form>
             </div>
--- admin/pages/Order/OrderLogic.php
+++ admin/pages/Order/OrderLogic.php
@@ -1,8 +1,14 @@
 <?php
 include "../../../common/config/Connect.php";
-$madonhang = $_POST['madonhang'];
-$idkhachhang = $_POST['idkhachhang'];;
-$hinhthucthanhtoan = $_POST['hinhthucthanhtoan'];
+$userId  = "SELECT id from tbl_user;";
+$payment_type_id = "SELECT id from tbl_payment_type";
+$status_id = "SELECT id  from tbl_status";
+$dienthoainhan = $_POST['dienthoainhan'];
+$diachinhan = $_POST['diachinhan'];
+$phigiaohang = $_POST['phigiaohang'];
+$mota = $_POST['mota'];
+
+
 function generateUuid()
 {
     $data = random_bytes(16);
@@ -10,21 +16,20 @@ function generateUuid()
     // Set the version (4) and variant bits (2)
     $data[6] = chr(ord($data[6]) & 0x0F | 0x40);
     $data[8] = chr(ord($data[8]) & 0x3F | 0x80);
-
     // Format the UUID string
     $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-
     return $uuid;
 }
 if(isset($_POST['addOrder'])){
     $Id =  generateUuid();
-    $add = "INSERT INTO tbl_giohang(id_cart,code_cart,id_khachhang,cart_payment) VALUES ('".$Id."','".$madonhang. "','".$idkhachhang."','".$hinhthucthanhtoan."')";  
+    $add = "INSERT INTO tbl_order(id,user_id,payment_type_id,status_id,receive_phone,receive_address_delivery_cost,discription) 
+    VALUES ('".$Id."','".$userId. "','".$dienthoainhan."','".$diachinhan."','".$phigiaohang."','".$mota."')";  
     mysqli_query($connect, $add);
     header('Location:../../AdminIndex.php?workingPage=order');
 }
 else if (isset($_POST['delete'])) {
     $id = $_GET['orderId'];
-    $sql_xoa = "DELETE FROM tbl_giohang WHERE id_cart ='".$id."'";
+    $sql_xoa = "DELETE FROM tbl_order WHERE id ='".$id."'";
     mysqli_query($connect, $sql_xoa);
     header('Location:../../AdminIndex.php?workingPage=order');
 }
