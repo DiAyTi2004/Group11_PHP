6052744157b13f10440ceb67117170257870d313
Nathan10252 <hoangvuong10252@gmail.com>
2023-11-11 17:56:43 +0700

    add status management

--- admin/adminCommon/AdminRouter.php
+++ admin/adminCommon/AdminRouter.php
@@ -7,11 +7,11 @@
         } else if ($workingPage == 'product') {
             include("./pages/Product/ProductIndex.php");
             include("./pages/Product/AddProductPopup.php");
-        } else if ($workingPage == 'event' && $query == 'edit') {
-            include("./pages/Event/EditEventPopup.php");
-        } else if ($workingPage == 'event') {
+        } 
+        else if ($workingPage == 'event') {
             include("./pages/Event/EventIndex.php");
             include("./pages/Event/AddEventPopup.php");
+            include("./pages/Event/EditEventPopup.php");
             include("./pages/Event/ConfirmDeleteEventPopup.php");
         } else if ($workingPage == 'user') {
             include("./pages/User/UserIndex.php");
@@ -22,6 +22,12 @@
             include("./pages/Order/OrderIndex.php");
             include("./pages/Order/AddOrderPopup.php");
             include("./pages/Order/OrderConfirmDelete.php");
+        }
+        else if ($workingPage == 'status') {
+            include("./pages/Status/StatusIndex.php");
+            include("./pages/Status/AddStatusPopup.php");
+            include("./pages/Status/EditStatusPopup.php");
+            include("./pages/Status/ConfirmDeleteStatusPopup.php");
         } else if ($workingPage == 'dangxuat') {
             include("Login.php");
         } else {
