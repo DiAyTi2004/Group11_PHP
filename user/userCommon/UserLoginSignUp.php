fea5b21bdbd15bbf18708628a64f208730e2b3c3
BaVi Cao <114662682+bavilv123@users.noreply.github.com>
2023-11-18 18:34:59 +0700

    Sửa logo

--- user/userCommon/UserLoginSignUp.php
+++ user/userCommon/UserLoginSignUp.php
@@ -99,10 +99,10 @@ if (isset($_POST['login']) && isset($_POST['username']) && isset($_POST['passwor
 
 <body>
 <a href="UserIndex.php">
-    <img src="./images/logo.svg" alt="" style="width: 120px;position: absolute; top: 0 ; right: 10px" >
+    <img src="./images/logo.svg" alt="" style="width: 120px;position: absolute; top: 35px ; right: 10px" >
 </a>
         
-        <a href="../../UserIndex.php" class="p-2 bg-white" style="border-radius: 10px; position: absolute;top: 0;left:10px;font-weight: bold;">
+        <a href="../../UserIndex.php" class="p-2 bg-white" style="border-radius: 10px; position: absolute;top: 35px;left:10px;font-weight: bold;">
             <i class="fa-solid fa-circle-chevron-left"></i>
             Về trang chủ
         </a>
