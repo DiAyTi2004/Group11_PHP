61a95a3c99cc61f3d838d209d81b45c4614c13e3
NguyenDuongPhong <145484926+NguyenDuongPhong@users.noreply.github.com>
2023-11-18 19:08:51 +0700

    insert data

--- shoes_land.sql
+++ shoes_land.sql
@@ -283,6 +283,13 @@ create table tbl_payment_type (
   code varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   description LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+insert into tbl_payment_type values ('cc01ac4e-e003-430c-9076-236b55a8991c','Tiền mặt','#TM','Thanh toán trực tiếp bằng tiền mặt là một phương thức phổ biến, đặc biệt là trong các giao dịch hàng ngày, mua sắm tại cửa hàng truyền thống hoặc chợ.');
+insert into tbl_payment_type values ('78e09f60-bb2b-4bc7-86c2-ca27a33aa5c8','Chuyển khoản ngân hàng','#CKNN','Người mua có thể chuyển tiền trực tiếp từ tài khoản ngân hàng của họ sang tài khoản của người bán. Phương thức này thường được sử dụng cho các giao dịch lớn và trực tuyến.');
+insert into tbl_payment_type values ('2e198743-d03d-43db-ac59-3f2bb47d5de3','Thẻ tín dụng và thẻ ghi nợ','#TTD&GN','Thanh toán bằng thẻ tín dụng hoặc thẻ ghi nợ là một phương thức phổ biến cho mua sắm trực tuyến và offline. Visa và MasterCard là hai loại thẻ phổ biến tại Việt Nam.');
+insert into tbl_payment_type values ('fdc8f73c-e3f6-4cb9-add0-c5a661563c9d','Ví điện tử','#VĐT','Sử dụng ví điện tử như MoMo, ZaloPay, ViettelPay, AirPay là cách thanh toán tiện lợi và ngày càng phổ biến. Người dùng có thể liên kết ví điện tử với tài khoản ngân hàng hoặc thẻ tín dụng để thực hiện thanh toán.');
+insert into tbl_payment_type values ('8d3c67e6-a124-4a6d-857d-869815ad189a','Chuyển phát nhanh COD','#COD','Trong trường hợp mua sắm trực tuyến, người mua có thể thanh toán khi nhận hàng, tiền mặt hoặc qua thẻ tín dụng. Phương thức này giúp người mua kiểm tra sản phẩm trước khi thanh toán.');
+insert into tbl_payment_type values ('790a1183-21d3-4ca5-bb84-1f8d7349b014','Internet Banking và Mobile Banking','#BANKING','Sử dụng dịch vụ ngân hàng trực tuyến qua Internet Banking hoặc ứng dụng Mobile Banking để thực hiện các giao dịch thanh toán.');
+insert into tbl_payment_type values ('64473054-f544-4a9c-9480-3c1a4e13df1e','Thanh toán qua QR Code','#QR','Sử dụng ứng dụng ví điện tử hoặc ứng dụng thanh toán trực tuyến để quét mã QR Code và thực hiện thanh toán.');
 
 create table tbl_size (
   id varchar(36) primary key not null,
@@ -290,10 +297,19 @@ create table tbl_size (
   name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   description LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-insert into tbl_size values ('b5e8cf2d-8b4b-4ecf-a7c1-63e7a0e3d1a9', 'S40', 'Cỡ 40', 'Kích thước giày 40 thường được coi là phổ biến và phù hợp với nhiều người, nhưng sự thoải mái và vừa vặn có thể phụ thuộc vào kiểu giày cũng như cấu trúc chân của mỗi người.');
-insert into tbl_size values ('daa2e4bd-68cf-4a9e-bb69-89a7a6df7c12', 'S41', 'Cỡ 41', 'Size giày 41 (EU) thường là một lựa chọn phổ biến cho những người có chân lớn hơn, và nó cung cấp một số ưu điểm nhất định.');
-insert into tbl_size values ('fc7f0e9b-0c8d-46ab-b8d5-1e38c34c738d', 'S42', 'Cỡ 42', 'Size giày 42 (EU) là một lựa chọn lớn hơn và thường phù hợp với những người có chân to hoặc mong muốn sự thoải mái và không gian trong giày.');
+insert into tbl_size values ('a1b14f82-848e-11ee-b962-0242ac120002', 'S35', 'Cỡ 35', 'Đây thường là những size dành cho người có bàn chân nhỏ và nhẹ. Size 35 thường là size phổ biến cho giày dành cho nữ.');
+insert into tbl_size values ('2dd58a8c-848f-11ee-b962-0242ac120002', 'S36', 'Cỡ 36', 'Đây thường là những size dành cho người có bàn chân nhỏ và nhẹ. Size 36 thường là size phổ biến cho giày dành cho nữ.');
+insert into tbl_size values ('35a34d94-848f-11ee-b962-0242ac120002', 'S37', 'Cỡ 37', 'Đây thường là những size dành cho người có bàn chân nhỏ và nhẹ. Size 37 thường là cho cả nam và nữ.');
+insert into tbl_size values ('3cb72b14-848f-11ee-b962-0242ac120002', 'S38', 'Cỡ 38', 'Đây thường là những size dành cho người có bàn chân nhỏ và nhẹ. Size 38 thường là cho cả nam và nữ.');
+insert into tbl_size values ('45ef4fc2-848f-11ee-b962-0242ac120002', 'S39', 'Cỡ 39', 'Đây thường là những size dành cho người có bàn chân nhỏ và nhẹ. Size 39 thường là cho cả nam và nữ.');
+insert into tbl_size values ('4c309e18-848f-11ee-b962-0242ac120002', 'S40', 'Cỡ 40', 'size này phổ biến cho người có bàn chân vừa đến lớn. Size 40 thường là size trung bình, phù hợp cho nhiều người.');
+insert into tbl_size values ('525d7ba8-848f-11ee-b962-0242ac120002', 'S41', 'Cỡ 41', 'size này phổ biến cho người có bàn chân vừa đến lớn. Size 41 thường là size trung bình, phù hợp cho nhiều người.');
+insert into tbl_size values ('59ad58ce-848f-11ee-b962-0242ac120002', 'S42', 'Cỡ 42', 'Size giày 42 là một lựa chọn lớn hơn và thường phù hợp với những người có chân to hoặc mong muốn sự thoải mái và không gian trong giày.');
+insert into tbl_size values ('5f2518c8-848f-11ee-b962-0242ac120002', 'S43', 'Cỡ 43', 'Size giày 43 là một lựa chọn lớn hơn và thường phù hợp với những người có chân to hoặc mong muốn sự thoải mái và không gian trong giày.');
+insert into tbl_size values ('6bfdd788-848f-11ee-b962-0242ac120002', 'S44', 'Cỡ 44', 'Size này thường dành cho những người có bàn chân lớn và mạnh mẽ. Size 44 thường là phổ biến cho nam giới.');
+insert into tbl_size values ('72d16070-848f-11ee-b962-0242ac120002', 'S45', 'Cỡ 45', 'Size này thường dành cho những người có bàn chân lớn và mạnh mẽ. Size 45 thường là phổ biến cho nam giới.');
+insert into tbl_size values ('7836460c-848f-11ee-b962-0242ac120002', 'S46', 'Cỡ 46', 'Size này thường dành cho những người có bàn chân lớn và mạnh mẽ. Size 46 thường là cho những người có bàn chân cực kỳ lớn.');
+insert into tbl_size values ('809a4cf8-848f-11ee-b962-0242ac120002', 'S47', 'Cỡ 47', 'Size này thường dành cho những người có bàn chân lớn và mạnh mẽ. Size 47 thường là cho những người có bàn chân cực kỳ lớn.');
 
 create table tbl_category (
   id varchar(36) primary key not null, 
@@ -304,7 +320,16 @@ create table tbl_category (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO tbl_category (id, code, name, category_image , description) VALUES
-('a4f18b20-7b62-4c88-8b03-3a9c1c2d5eab', 'NIKE', 'Nike','nikecategory.jpg', 'Nike là một trong những thương hiệu thể thao nổi tiếng toàn cầu, được biết đến với việc sản xuất và phân phối đa dạng sản phẩm thể thao và thời trang.');
+('a4f18b20-7b62-4c88-8b03-3a9c1c2d5eab', 'NIKE', 'Nike','nikecategory.jpg', 'Nike là một trong những thương hiệu thể thao nổi tiếng toàn cầu, được biết đến với việc sản xuất và phân phối đa dạng sản phẩm thể thao và thời trang.'),
+('582d296d-37f7-4b0e-81ca-ff311581c2b6','ADIDAS','Adidas','adidas.png','Giày Adidas là một dòng sản phẩm giày thể thao và thời trang của thương hiệu Adidas, một trong những nhãn hiệu nổi tiếng hàng đầu trên thế giới. Adidas đã tạo ra nhiều mẫu giày khác nhau, phục vụ nhu cầu của người tiêu dùng từ các hoạt động thể thao đến thời trang hàng ngày.'),
+('1ab78998-fa75-4abb-b937-4e59a483c171','CONVERSE','Converse','Converse.png','Giày Converse là những đôi giày thể thao mang đậm phong cách retro và văn hóa đô thị. Dòng giày nổi tiếng nhất của Converse là Chuck Taylor All Star, một biểu tượng thời trang đã tồn tại từ những năm 1920 và trở thành một trong những mẫu giày phổ biến nhất trên thế giới.'),
+('aa3166a2-6534-40a1-a1a6-cc6839cfa666','JORDAN','Jordan','jordan.png','Giày Jordan là một dòng sản phẩm giày thể thao và thời trang của thương hiệu Jordan Brand, một phần của tập đoàn Nike. Dòng giày này được đặt tên theo huyền thoại bóng rổ Michael Jordan, người đã trở thành một trong những cầu thủ xuất sắc nhất trong lịch sử môn thể thao này.'),
+('9a4ab55e-3844-4d25-b20a-893b1bf7c980','MLB','Mlb','MLB.png','Mẫu giày được ưa chuộng trong giới baseball, được sử dụng trong các trận đấu Major League Baseball (MLB) hoặc được thiết kế với phong cách và ý tưởng liên quan đến bóng chày.'),
+('31895546-7989-4d6b-b45e-dd507da913c7','NEW BALANCE','New Balance','newbalance.png','Giày New Balance là sản phẩm của thương hiệu giày thể thao New Balance, một trong những nhãn hiệu nổi tiếng và đáng tin cậy trên thế giới.'),
+('31c124fe-dfdf-4777-aecb-895449fc3cbe','PUMA','Puma','puma.png','Puma là một trong những thương hiệu giày và thể thao hàng đầu trên thế giới, có trụ sở chính tại Herzogenaurach, Đức.'),
+('99d98fa2-5448-4245-ab31-1fdc94cd727d','TODS','Tods','tods.png','Tods là một thương hiệu thời trang và giày dép cao cấp có trụ sở tại Ý, nổi tiếng với việc sản xuất giày da chất lượng cao và các sản phẩm thời trang sang trọng.'),
+('90261ccc-7523-46c4-a50d-bf444c6ef699','VANS','Vans','vans.png','Vans là một thương hiệu giày và thời trang đường phố nổi tiếng, xuất phát từ California, Hoa Kỳ. Đặc biệt là với dòng giày skate và lifestyle.'),
+('3e1b10c3-9fce-4e00-8304-850cefb45b55','YEEZY','Yeezy','yeezy.png','Yeezy là kết quả của sự hợp tác giữa Kanye West và Adidas, một trong những thương hiệu thể thao hàng đầu trên thế giới. Sự kết hợp giữa tầm nhìn sáng tạo của Kanye West và kỹ thuật sản xuất của Adidas đã tạo ra các sản phẩm độc đáo và độc lập.');
 
 create table tbl_event (
   id varchar(36) primary key not null,
@@ -316,6 +341,18 @@ create table tbl_event (
   description LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   banner varchar(100)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+insert into tbl_event values ('8d7a68e9-1783-4aff-8c71-1814bcdbda46','2023-10-01 08:00:00 SA','2023-10-30 22:00:00 CH','50%','#1','Ưu đãi tháng 10','Sự kiện giảm giá và khuyến mãi đặc biệt cho mùa đông đã tới.','even1.png');
+insert into tbl_event values ('c271c6ed-6926-4be9-afc6-b11b3e957ef3','2023-10-15 08:00:00 SA','2023-10-30 22:00 CH','70%','#2','Flash Sale','Sale giá sốc-giảm đến 70%, freeship từ 2 đôi.','even2.png');
+insert into tbl_event values ('2d5d30f9-066f-4839-a973-2aa971803024','2023-11-01 08:00:00 SA','2023-11-10 22:00 CH','60%','#3','Ra mắt Sản phẩm Mới','Ra mắt sản phẩm mới cực hot. Bốc thăm giảm giá cực sốc cho 5 khách hàng may mắn.','even3.png');
+insert into tbl_event values ('452b9451-e86e-4048-a6ee-0d13dc857483','2023-11-11 08:00:00 SA','2023-11-21 22:00 CH','40%','#4','Mua càng nhiều giảm càng nhiều','Mua từ 3 đôi trở lên nhận giảm giá cực sốc.','even4.png');
+insert into tbl_event values ('f2822729-c278-4b25-9aba-6f2bd1a02133','2023-12-01 08:00:00 SA','2023-12-16 22:00 CH','30%','#5','Chương trình mua giày cặp đôi','Khách hàng là cặp đôi sẽ nhận về ưu đãi, giảm giá và quà tặng giá trị.','even5.png');
+insert into tbl_event values ('afcd72e9-6ec2-4d47-8a82-50477d6216dd','2023-12-17 08:00:00 SA','2023-12-30 22:00 CH','30%','#6','Ngày lễ kỉ niệm','Lễ kỉ niệm 40 năm của hãng Bitit, giảm giá sốc với các sản phẩm hãng Bitit.','even6.png');
+insert into tbl_event values ('4cf28b83-1694-4e3d-af7b-5c0b3c4780a0','2023-10-13 08:00:00 SA','2023-10-14 22:00 CH','50%','#7','Sự kiện BlackFriday','Giảm giá cực sốc khi khách hàng đến với shoesland vào dịp này.','even7.jpg');
+insert into tbl_event values ('2bc0c41b-0c08-43b7-8c45-d2f8c5c1566e','2023-12-23 00:00:00 SA','2023-12-25 23:59 CH','50%','#8','Ngày lễ Giáng Sinh','Giảm giá cực sốc đối với các sản phẩm có màu đỏ và xanh lá.','even8.jpg');
+insert into tbl_event values ('d3950d8f-7e44-4786-8caf-f480f2b34540','2023-12-27 08:00:00 SA','2024-01-03 22:00 CH','50%','#9','Tết dương lịch','Giảm giá cực sốc khi khách hàng đến với shoesland vào dịp này.','even9.png');
+insert into tbl_event values ('11afef6c-bff6-432b-82eb-d44ba44743e2','2024-02-08 00:00:00 SA','2024-02-14 23:59 CH','50%','#10','Tết Nguyên Đán','Giảm giá cực sốc khi khách hàng đến với shoesland vào dịp này.','even10.png');
+
+
 
 create table tbl_product (
   id varchar(36) primary key not null,
@@ -359,7 +396,18 @@ create table tbl_status (
   name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   description LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+insert into tbl_status (id, code, name, description) value 
+('f1e30780-f494-477d-8fba-63d280843c91','S1','Chờ xác nhận','Trạng thái đầu tiên khi khách hàng đặt hàng thành công và hệ thống ghi nhận thông tin đơn hàng.'),
+('94ea518b-b5cf-4ee3-8f8b-65a73c33a7e8','S2','Đã xác nhận','Khách hàng nhận được xác nhận đơn hàng qua email hoặc tin nhắn, xác nhận rằng đơn hàng của họ đã được nhận và đang được xử lý.'),
+('1221802e-1ad2-4aba-91ae-69b86270ae32','S3','Đang đóng gói','Đơn hàng bắt đầu được xử lý và chuẩn bị để đóng gói.'),
+('30928569-9d4c-4e24-a2f5-447a1cc895b5','S4','Chờ lấy hàng','Sản phẩm đã đóng gói xong và chờ đối tác vận chuyển đến lấy.'),
+('3f8c1efb-7b9e-4bdd-a856-d45e01053585','S5','Chờ giao hàng','Sản phẩm được đóng gói cẩn thận và chuyển giao cho đối tác vận chuyển để giao hàng.'),
+('6e5f0c4f-bec1-4766-9af6-8f5928f56287','S6','Đang giao hàng','Đơn hàng đã rời kho và đang trên đường để được giao đến địa chỉ của khách hàng.'),
+('2fb6fe9b-c49c-4358-8dc1-943e20f5f094','S7','Đã giao hàng','Đơn hàng đã được giao thành công đến tay khách hàng.'),
+('10dcaad2-8c9e-4078-85b4-8fbd6ed50c26','S8','Đã hủy','Trạng thái này xảy ra khi đơn hàng bị hủy bởi khách hàng hoặc do các lý do khác.'),
+('00c64206-1c21-4fb8-b679-007de67d76c9','S9','Trả hàng','Đơn hàng đã được trả lại do lý do nào đó, và quá trình xử lý đơn hàng trả lại bắt đầu.'),
+('6e4134f7-7ef4-4244-8a6e-4ae3ac5bbfa5','S10','Hoàn tiền','Khi đơn hàng được trả lại, quá trình hoàn tiền được thực hiện nếu có.'),
+('b0b461c2-2309-4b2a-b186-f415368105e7','S11','Hoàn thành','Trạng thái cuối cùng của đơn hàng khi tất cả các quy trình liên quan đã được hoàn tất.');
 create table tbl_order (
   id varchar(36) primary key not null,
   code varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
