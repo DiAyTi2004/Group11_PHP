5a5ed1e136e18e914fb6e12031b91d43a13d7254
tutai307 <tutaihocgioi@gmail.com>
2023-11-23 16:06:01 +0700

    details product

--- user/pages/Home/NewProductSection.php
+++ user/pages/Home/NewProductSection.php
@@ -1,19 +1,21 @@
 <?php
 // GET id là lấy id từ bên MENU.php 
-$sql_show_new = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc AND tbl_sanpham.trangthai=1 ORDER BY tbl_sanpham.id_sanpham DESC LIMIT 5";
-$query_show_new = mysqli_query($connect, $sql_show_new);
+
+$sql_show_test = "SELECT * FROM tbl_product INNER JOIN tbl_product_image ON tbl_product.id = tbl_product_image.product_id
+WHERE tbl_product_image.main_image = 1";
+$query_show_test = mysqli_query($connect, $sql_show_test);
 ?>
 
 <ul class="product_list">
     <?php
-    while ($row = mysqli_fetch_array($query_show_new)) {
+    while ($row_test = mysqli_fetch_array($query_show_test)) {
     ?>
         <li>
-            <a href="UserIndex.php?usingPage=product&id=<?php echo $row['id_sanpham'] ?>">
-                <img src="../../admin/pages/Product/ProductImages/<?php echo $row['hinhanh'] ?>">
+            <a href="UserIndex.php?usingPage=product&id=<?php echo $row_test['product_id'] ?>">
+                <img src="../../admin/pages/ProductImage/<?php echo $row_test['content'] ?>">
                 <p></p>
-                <h5 class="title_product"> <?php echo $row['tensanpham'] ?></h5>
-                <h5 class="price_product">Giá: <?php echo number_format($row['giasanpham'], 0, ',', '.') . ' VNĐ' ?></h5>
+                <h5 class="title_product"> <?php echo $row_test['name'] ?></h5>
+                <h5 class="price_product">Giá: <?php echo number_format($row_test['price'], 0, ',', '.') . ' VNĐ' ?></h5>
                 <p style="text-align: center;"><?php echo "Xem chi tiết" ?></p>
             </a>
         </li>
