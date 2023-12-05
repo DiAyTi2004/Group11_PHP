065041e1878d84b4550f2a3391f2f607c30c4ee3
tutai307 <tutaihocgioi@gmail.com>
2023-12-05 23:48:31 +0700

    commit medium

--- user/pages/Cart/CartIndex.php
+++ user/pages/Cart/CartIndex.php
@@ -50,6 +50,8 @@ $cart_empty = mysqli_num_rows($show_cart_query) == 0; // Check if the cart is em
                         $show_image_query = mysqli_query($connect, $show_image_sql);
                         $row_image = mysqli_fetch_array($show_image_query);
 
+                        $totalPrice = $row_cart['unit_price'] * $row_cart['quantity'];
+
                         $rowCartId = $_COOKIE['cartId'];
                         $rowProductId = $row_cart['product_id'];
                         $rowSizeId = $row_cart['size_id'];
@@ -80,18 +82,19 @@ $cart_empty = mysqli_num_rows($show_cart_query) == 0; // Check if the cart is em
                                 </span>
                             </td>
                             <td class="text-center align-middle">
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button" class="btn btn-primary btn-decrease">-</button>
-                                    <input class="btn btn-outline-primary" id="quantity" name="quantity[]" size="1" min="1" max="5" value="<?php echo $row_cart['quantity'] ?>">
-                                    <button type="button" class="btn btn-primary btn-increase">+</button>
-                                </div>
+                                <?php echo $row_cart['quantity'] ?>
                             </td>
 
                             <td class="text-center align-middle">
                                 <span class="price_real" id="total_Price">
+                                    <?php echo number_format($totalPrice, 0, ',', '.') . ' đ' ?>
+
                                 </span>
                             </td>
                             <td class="text-center align-middle">
+                                <button type="button" class="btn btn-primary edit-btn" data-bs-toggle="modal" data-bs-target="#editPopup_<?php echo $row_cart['product_id']; ?>_<?php echo $row_cart['size_id']; ?>">
+                                    <i class="fa-solid fa-pencil"></i>
+                                    Chỉnh sửa</button>
                                 <button type="button" class="btn btn-danger remove-btn" data-bs-toggle="modal" data-bs-target="#confirmPopup_<?php echo $row_cart['product_id']; ?>">
                                     <i class="fa-solid fa-trash"></i>
                                     Xoá</button>
@@ -101,16 +104,12 @@ $cart_empty = mysqli_num_rows($show_cart_query) == 0; // Check if the cart is em
                     }
                     ?>
                 </table>
-                <!-- <div class="ml-2 mb-3 form-check">
-                    <input name="submit" type="checkbox" class="form-check-input" id="exampleCheck1" required>
-                    <label class="form-check-label" for="exampleCheck1">Bạn có muốn xác nhận đơn hàng này?</label>
-                </div> -->
             </div>
 
             <input type="hidden" name="totalAmount">
 
             <div class="text-end mt-3">
-                Tổng số tiền: <span id="totalAmountDisplay" class="price_real"> <?php echo number_format($totalAmount, 0, ',', '.') . ' đ'; ?></span>
+                Tổng số tiền: <span id="totalAmountDisplay" class="price_real"></span>
                 <button name="confirmBuy" type="submit" class="btn btn-success p-3" id="buyButton">
                     <i class="fa-solid fa-cart-shopping mr-2 ml-0"></i>
                     Mua hàng
@@ -132,51 +131,23 @@ $cart_empty = mysqli_num_rows($show_cart_query) == 0; // Check if the cart is em
                     updateTotalAmount();
                 });
 
-                // Increase quantity button click
-                $('.btn-increase').click(function() {
-                    var quantityElement = $(this).closest('tr').find('.text-center.align-middle:eq(4) input');
-                    var currentQuantity = parseInt(quantityElement.val());
-                    quantityElement.val(currentQuantity + 1);
-                    updateTotalAmount();
-                });
-
-                // Decrease quantity button click
-                $('.btn-decrease').click(function() {
-                    var quantityElement = $(this).closest('tr').find('.text-center.align-middle:eq(4) input');
-                    var currentQuantity = parseInt(quantityElement.val());
-                    if (currentQuantity > 1) {
-                        quantityElement.val(currentQuantity - 1);
-                        updateTotalAmount();
-                    }
-                });
-
                 // Function to update total amount based on selected checkboxes
                 function updateTotalAmount() {
                     var totalAmount = 0;
 
-                    // Loop through all items
-                    $('input[name="selectedPro[]"]').each(function() {
-                        var quantity = parseInt($(this).closest('tr').find('.text-center.align-middle:eq(4) input').val());
+                    $('input[name="selectedPro[]"]:checked').each(function() {
+                        // Update the indices used to access quantity and unit price
+                        var quantity = parseInt($(this).closest('tr').find('.text-center.align-middle:eq(4)').text());
                         var unitPriceText = $(this).closest('tr').find('.text-center.align-middle:eq(3) .price_real').text();
 
                         // Remove non-numeric characters except for the dot
                         var unitPrice = parseInt(unitPriceText.replace(/[^\d]/g, ''));
 
-                        // Update total_Price display for each item individually
-                        var total_Price = quantity * unitPrice;
-                        $(this).closest('tr').find('.text-center.align-middle:eq(5) #total_Price').text(formatCurrency(total_Price) + ' đ');
-
-                        // Update totalAmount only for checked items
-                        if ($(this).is(':checked')) {
-                            totalAmount += total_Price;
-                        }
+                        totalAmount += quantity * unitPrice;
                     });
 
                     // Update the display
                     $('#totalAmountDisplay').text(formatCurrency(totalAmount) + ' đ');
-
-                    // Update the value in the form
-                    $('input[name="totalAmount"]').val(totalAmount);
                 }
 
                 // Function to format currency with commas
@@ -185,11 +156,20 @@ $cart_empty = mysqli_num_rows($show_cart_query) == 0; // Check if the cart is em
                 }
             });
         </script>
+        <!-- pre delete -->
         <?php
         $show_cart_query = mysqli_query($connect, $show_cart_sql);
 
         while ($row_cart = mysqli_fetch_array($show_cart_query)) {
             include "../../user/pages/Cart/CartConfirmDeletePopup.php";
+        }
+        ?>
+        <!-- pre edit -->
+        <?php
+        $show_cart_query = mysqli_query($connect, $show_cart_sql);
+
+        while ($row_cart = mysqli_fetch_array($show_cart_query)) {
+            include "../../user/pages/Cart/EditQuantityPopup.php";
         }
         ?>
     <?php endif; ?>
--- user/pages/Cart/CartLogic.php
+++ user/pages/Cart/CartLogic.php
@@ -10,14 +10,12 @@ if (isset($_POST['deleteProduct'])) {
     header('Location:../../userCommon/UserIndex.php?usingPage=cart');
 } else if (isset($_POST['confirmBuy'])) {
     //viết logic để tạo ra 1 order
-    
-
-
-
     // var_dump($_POST['selectedPro']);
     $selectedPro = $_POST['selectedPro'];
 
     foreach ($selectedPro as $item) {
+        $order_id = generateUuid();
+        $order_code = generateCode();
         echo "các dòng được chọn: \n";
         $tachChuoi = explode("diayti", $item);
 
@@ -30,4 +28,19 @@ if (isset($_POST['deleteProduct'])) {
         echo "productId: $productId<br>";
         echo "sizeId:  $sizeId<br>";
     }
+} else if(isset($_POST['editCart'])) {
+    $productId = $_GET['productId'];
+    $newQuantity = $_POST['quantity'];
+    $newSize = $_POST['size'];
+    $updateSql = "
+    UPDATE tbl_cart_detail
+    SET 
+    quantity = '$newQuantity',
+    size_id = '$newSize'
+    WHERE product_id='$productId' AND cart_id = '$_COOKIE[cartId]';
+    ";
+
+    mysqli_query($connect, $updateSql);
+
+    header('Location:../../userCommon/UserIndex.php?usingPage=cart');
 }
