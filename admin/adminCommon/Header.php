dd55da4f0b9cd9f1d78e8cb1faabfcbf683f79e4
BaVi Cao <114662682+bavilv123@users.noreply.github.com>
2023-12-07 19:14:29 +0700

    Thêm popUp xác nhận cho user và admin

--- admin/adminCommon/Header.php
+++ admin/adminCommon/Header.php
@@ -1,4 +1,5 @@
-<header>
+<header><script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <div class="px-3 bg-dark text-white">
         <div class="container px-0">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -86,7 +87,32 @@
                         </a>
                     </li>
                     <li class="flex-center">
-                        <a href="adminCommon/Login.php" class="pr-0 nav-link text-white flex-column flex-center">
+                        <div class="text-end">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                <i class="fa-solid fa-right-from-bracket mr-1"></i>
+                                Đăng xuất
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Đăng xuất</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+
+            </div>
+            <div class="modal-body">
+                <p>Bạn có muốn đăng xuất không? </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Không</button>
+                <a href="adminCommon/Login.php" class="pr-0 nav-link text-white flex-column flex-center">
                             <div class="text-end">
                                 <?php
                                 if (isset($_GET['logout']) && $_GET['logout'] == 1) {
@@ -100,9 +126,24 @@
                                 </button>
                             </div>
                         </a>
-                    </li>
-                </ul>
+
+                </button>
             </div>
         </div>
     </div>
-</header>
+</div>
+<style>
+    #myModal .modal-header {
+        background-color: #28a745;
+        /* Green background color */
+        color: #fff;
+        /* White text color */
+    }
+
+    #myModal .btn-primary {
+        background-color: green;
+        /* Red background color */
+        color: #fff;
+        /* White text color */
+    }
+</style>
