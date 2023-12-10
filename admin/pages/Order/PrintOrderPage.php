e2c6e7f5e93d5f9aa194a3a98287eeee70ae2ce6
Nathan10252 <hoangvuong10252@gmail.com>
2023-12-11 02:52:26 +0700

    update print order admin

--- admin/pages/Order/PrintOrderPage.php
+++ admin/pages/Order/PrintOrderPage.php
@@ -15,8 +15,8 @@ $currentYear = date('Y');
             <div class="headerPrintOrder flex-center justify-between" style="padding: 0px 40px;">
                 <div class="logo w-300 h-300">
                     <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/385531793_665092928850659_9049026433017091781_n.jpg?stp=dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=510075&_nc_ohc=lgHPtfIooO0AX9Lgia2&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSJ0IjzJmukql67fujDBhjSqxH66Y9EJQSe_IEKM58l9Q&oe=659C4C0D" alt="logo">
-            </div>
-            <div class=" storeInfo text-center">
+                </div>
+                <div class=" storeInfo text-center">
                     <h1 style="text-align: center;">HÓA ĐƠN BÁN HÀNG</h1>
                     <h4><i class="fa-solid fa-square-phone"></i> HOTLINE: 01675984726</h4>
                     <h4><i class="fa-solid fa-location-dot"></i> 210 Lê Trọng Tấn, Thanh Xuân, Hà Nội</h4>
@@ -27,14 +27,14 @@ $currentYear = date('Y');
                 <div class="orderInfo">
                     <div class="flex mb-3">
                         <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis; display: inline-block; width: 100%; margin-bottom: 5px;">
-                            <strong>Mã Đơn Hàng: </strong> <?php echo $row['code'] . str_repeat(' .', 100);?>
+                            <strong>Mã Đơn Hàng: </strong> <?php echo $row['code'] . str_repeat(' .', 100); ?>
                         </p>
                         <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis; display: inline-block; width: 100%; margin-bottom: 5px;" class='justify-right'>
-                            <strong>Điện Thoại Nhận Hàng:</strong> <?php echo $row['receive_phone'] . str_repeat(' .', 100);?>
+                            <strong>Điện Thoại Nhận Hàng:</strong> <?php echo $row['receive_phone'] . str_repeat(' .', 100); ?>
                         </p>
                     </div>
                     <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;"><strong>Địa Chỉ Nhận:</strong> <?php echo $row['receive_address'] . str_repeat(' .', 100); ?></p>
-                    <p><strong>Ghi chú đơn hàng:</strong> <?php echo $row['description'] . str_repeat(' .', 200 - mb_strlen($row['description']));?></p>
+                    <p><strong>Ghi chú đơn hàng:</strong> <?php echo $row['description'] . str_repeat(' .', 200 - mb_strlen($row['description'])); ?></p>
                 </div>
                 <div class="product-table">
                     <table border="1" class="w-100 table">
